c6b56cd8d459314100c6827a119e93e5d1addd75
ren <0806renkun@gmail.com>
2023-01-17 18:07:06 +0900

    git

--- src/BlockBreakGame.java
+++ src/BlockBreakGame.java
@@ -1,14 +1,13 @@
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 public class BlockBreakGame {
-    public static JFrame frame;
+    JFrame frame;
+    static BlockBreakGame Class;
     CardLayout layout;
 
-    public static Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
+    Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
 
     TitlePanel titlePanel;
     GamePanel gamePanel;
@@ -19,6 +18,7 @@ public class BlockBreakGame {
     }
 
     BlockBreakGame() {
+        Class = this;
         frame = new JFrame("Block Break");
         frame.setSize(new Dimension(480, 640));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -38,9 +38,6 @@ public class BlockBreakGame {
         layout.show(this.frame.getContentPane(), "Title");
         titlePanel.requestFocus();
 
-        titlePanel.Init();
-        gamePanel.Init();
-
         Timer timer = new Timer(10, (e) -> update());
         timer.start();
     }
@@ -55,14 +52,12 @@ class TitlePanel extends JPanel {
     TitlePanel() {
         this.setBackground(Color.white);
         this.setLayout(null);
-    }
 
-    public void Init() {
         JLabel titleLabel = new JLabel("Block Break!!");
         titleLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 50));
-        titleLabel.setBounds(0, 100, BlockBreakGame.frame.getWidth(), 50);
+        titleLabel.setBounds(0, 100, BlockBreakGame.Class.frame.getWidth(), 50);
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
-        titleLabel.setBorder(BlockBreakGame.border);
+        titleLabel.setBorder(BlockBreakGame.Class.border);
         this.add(titleLabel);
     }
 
@@ -77,10 +72,6 @@ class GamePanel extends JPanel {
     GamePanel() {
         this.setBackground(Color.black);
         this.setLayout(null);
-    }
-
-    public void Init() {
-
     }
 
     public void Update() {
